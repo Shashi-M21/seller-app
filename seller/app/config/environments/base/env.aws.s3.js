1a0dd2995bdf546614369c8b9d28f977afa97a4a
Shashi-M21 <134912431+Shashi-M21@users.noreply.github.com>
2023-06-07 13:34:48 +0530

    Update utils

--- seller/app/config/environments/base/env.aws.s3.js
+++ seller/app/config/environments/base/env.aws.s3.js
@@ -1,21 +1,13 @@
-// module.exports = {
-//     s3: {
-//         accessKeyId: process.env.S3_ACCESS_KEY,
-//         secretAccessKey: process.env.S3_SECRET_KEY,
-//         region: process.env.S3_REGION,
-//         version: process.env.S3_VERSION,
-//         bucket: process.env.S3_BUCKET
-//     }
-// };
-
 module.exports = {
     s3: {
-        accessKeyId:'AKIAXHJWDJ7ENCEMODGX',
-        secretAccessKey:'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',
-        region:'ap-southeast-2',
-        signatureVersion:'v4',
-        bucket:'slio-miscellaneous'
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_KEY,
+        region: process.env.S3_REGION,
+        version: process.env.S3_VERSION,
+        bucket: process.env.S3_BUCKET
     }
 };
+
+
 
 
